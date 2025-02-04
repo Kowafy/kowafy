a26c50a0775ade80ef81ef53530eee7b58cf3b42
jjwaizna <asatsukki122638@gmail.com>
2025-02-04 16:04:27 +0300

    Update askname.c
    tg,jxsdryhzajrwag

--- askname.c
+++ askname.c
@@ -3,6 +3,7 @@
 
 void askname(char *first, char *last)
 {
+    gljfudesyksry
     printf("Enter your first name: ");
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
